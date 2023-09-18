8920dc2dddc28ac246d54165f728ae89de149dbc
Franco Rodriguez <Franco.d.r1992@gmail.com>
2023-09-18 17:48:51 -0300

    changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  mapBoxToken: 'pk.eyJ1IjoicHJvbWV0ZW9kaWUiLCJhIjoiY2xtODk4YmQ5MDdiZjNjbGQybW5zN3AyNSJ9._uHx0FJm4Xtd8j_gFAsbBg'
+  mapBoxToken: 'pk.eyJ1IjoicHJvbWV0ZW9kaWUiLCJhIjoiY2xtcGN5M3IyMTJ1ODJxbjV0OTU4OGc0aSJ9.9c1YM29Yu76om8aW4aMdcQ'
 };
